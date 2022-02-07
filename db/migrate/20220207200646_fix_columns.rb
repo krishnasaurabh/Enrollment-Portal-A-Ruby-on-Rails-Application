047c4b92d26d6bdaa2dbeb79fc9b21ebc56f1367
Atharva Patil <atharvapatil@Atharvas-MacBook-Pro.local>
2022-02-07 15:45:10 -0500

    updated migrations & fixed user managemenrt

--- db/migrate/20220207200646_fix_columns.rb
+++
@@ -1,5 +0,0 @@
-class FixColumns < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :users, :type, :user_type, :string
-  end
-end
--- db/migrate/20220207202149_fix_data_type.rb
+++ db/migrate/20220207202149_fix_data_type.rb
@@ -1,6 +1,5 @@
 class FixDataType < ActiveRecord::Migration[6.1]
   def change
-     remove_column :users, :user_type
      add_column :users, :user_type, :string
   end
 end

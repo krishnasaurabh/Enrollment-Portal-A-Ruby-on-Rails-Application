5febc035caff8f00ed5c5c1381f2784143b95d9a
kvankad <kvankad@ncsu.edu>
2022-02-15 17:47:34 -0500

    added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,22 @@ class ApplicationController < ActionController::Base
      end
      helper_method :is_admin?
 
+     def get_cur_student
+          if is_student?
+               return Student.find_by(user_id: current_user.id)
+          end
+          return nil
+     end
+     helper_method :get_cur_student
+
+     def get_cur_instructor
+          if is_instructor?
+               return Instructor.find_by(user_id: current_user.id)
+          end
+          return nil
+     end
+     helper_method :get_cur_instructor
+     
      protected
 
           def configure_permitted_parameters
